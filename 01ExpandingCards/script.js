941135a41344e5bd75f00d92219a3f683569f12e
david08thms <david08thms@gmail.com>
2024-08-28 01:25:34 -0300

    01 Expanding Cards end

---
+++ 01ExpandingCards/script.js
@@ -0,0 +1,22 @@
+// get all panels
+const panels = document.querySelectorAll('.panel')
+
+// add on click elements where active class is added
+panels.forEach( panel => {
+    panel.addEventListener('click', () => {
+
+        removeActiveClasses()
+
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+
+    panels.forEach( panel => {
+        panel.classList.remove('active')
+    })
+
+}
+
+
